36674b0e5c14ddec6d0f56fe1c75abfb32cd3fb3
yakuperenermurat <yakuperenermurat@gmail.com>
2024-05-20 17:31:23 +0300

    project finish

--- src/javas/locations/Mine.java
+++ src/javas/locations/Mine.java
@@ -6,6 +6,6 @@ import javas.obstacles.Snake;
 
 public class Mine extends BattleLoc {
     public Mine(Player player) {
-        super(player, "Maden", new Snake(), "tabak", 5);
+        super(player, "Maden", new Snake(), "Ganimet", 5);
     }
 }
